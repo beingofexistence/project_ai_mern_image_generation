f93dad6aae99476421e9bb1aad46a17dcdd38ad9
Being Of Existence <manfromearth25@gmail.com>
2023-08-19 19:58:32 +0000

    feat: hello

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+      const response = await fetch('https://8080-beingofexis-projectaime-gzzf76wuy7a.ws-us104.gitpod.io/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
